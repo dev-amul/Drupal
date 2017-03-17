9a439fa6558a542ba1d0618bc87f328e29f0cf31
dev-amul <amulc451@gmail.com>
2017-03-17 13:15:01 +0100

    322 - Context is needed in Views Exposed Filters

--- sites/all/modules/custom_modules/siasar_field_location/siasar_field_location.js
+++ sites/all/modules/custom_modules/siasar_field_location/siasar_field_location.js
@@ -3,8 +3,6 @@
 
   Drupal.behaviors.siasarFieldLocation = {
     attach: function (context, settings) {
-      if (context !== document) return;
-
       $('.field-widget-siasar-hierarchical-select, .views-exposed-form .form-item-field-entidad-local-tid', context).siasarHierarchicalSelect();
     }
   };
