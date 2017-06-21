57d62f016d5c6da039275647c9eeaca81b7962c4
dev-amul <amulc451@gmail.com>
2017-06-21 10:55:39 +0200

    #517: Fixed issue for selecting deeper location

--- sites/all/modules/custom_modules/siasar_field_location/siasar_field_location.plugin.js
+++ sites/all/modules/custom_modules/siasar_field_location/siasar_field_location.plugin.js
@@ -86,10 +86,6 @@
       };
 
       this.buildSelectorLevel = function (tid, level) {
-        if (!this.restrictions.can_change && level > this.restrictions.max_level) {
-          return;
-        }
-
         var html = '<select class="location-tree-selector" data-level="' + level + '"></select>';
         var $newSelector;
 
