2bcc4667c02be3d5e4434d6ce0cbf0dcac1ea49a
dev-amul <amulc451@gmail.com>
2017-07-24 02:10:22 -0300

    #495 - responsive menu

---
+++ sites/all/themes/siasar_material/js/responsiveMenu.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Contains helper functions to work with theme.
+ */
+
+(function ($) {
+    "use strict"
+
+    /**
+     * Build a responsive menu merging links from .main-menu and .user-menu.
+     */
+    Drupal.behaviors.responsiveMenu = {
+        attach: function (context) {
+          var menuToggle = $('<span />')
+          .addClass("responsive-menu-toggle")
+          .html('≡');
+          var regionUserNav = $(".region-user-nav");
+          menuToggle.prependTo(regionUserNav);
+          $('.responsive-menu-toggle').click(function(e) {
+            e.preventDefault();
+            $(".main-menu, .user-menu").toggle();
+          });
+        }
+    };
+})(jQuery);
