7d5834986063db826a968a7d83582ce69b092050
dev-amul <amulc451@gmail.com>
2017-08-14 16:31:58 +0200

    538 - Testing a bit deeper

--- tests/api/tests/formList.test.js
+++ tests/api/tests/formList.test.js
@@ -5,9 +5,10 @@ const request = require('../includes/request');
 
 
 it('API is listing form types', async () => {
-  expect.assertions(1);
+  expect.assertions(2);
   var data = await request.getOutput('forms');
   data = JSON.parse(data);
   expect(data).toHaveProperty('comunidad');
+  expect(data).toHaveProperty('sistema');
 });
 
