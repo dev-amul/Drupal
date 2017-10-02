b05a4cee5282b880f0ed81fb99fbeebc75f09b2f
dev-amul <amulc451@gmail.com>
2017-10-02 16:28:50 +0200

    #560 Export data in CSV improvements

--- sites/all/themes/siasar_material/template.php
+++ sites/all/themes/siasar_material/template.php
@@ -212,23 +212,3 @@ function siasar_material_workflow_history_table($variables) {
   }
   return $output;
 }
-
-/**
- * @see theme_views_data_export_feed_icon().
- */
-function siasar_material_views_data_export_feed_icon($variables) {
-  extract($variables, EXTR_SKIP);
-  $url_options = array('html' => TRUE);
-  if ($query) {
-    $url_options['query'] = $query;
-  }
-
-  $image = theme('image', array('path' => $image_path, 'alt' => $text, 'title' => $text));
-  //In case of the new export data, change the default icon
-  if (drupal_match_path($variables['url'],'resultados/com-ps-sys-pat')) {
-    $variables['image_path'] = drupal_get_path('theme', 'siasar_material') . '/img/nodes.svg';
-    $image = theme('image', array('path' => $variables['image_path'], 'alt' => $text, 'title' => $text, 'width' => '20px', 'height' => '14px'));
-  }
-
-  return l($image, $url, $url_options);
-}
